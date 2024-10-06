c81ca103855ae1207cb1629d49ad10ccaa893c89
bishwas350 <crashmindboy10@gmail.com>
2024-10-06 16:35:54 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          headerColor: '#1E1E1E',
+        },
+        // width 
+        maxWidth: {
+            'headerContainer': '1110px',
+            'bannerContainer': '1090px',
+            'aboutMe': '1108px',
+            'myService': '1110px',
+            'Contact': '1110px',
+          },
+        // width 
+
+        // font 
+        fontFamily: {
+          'lex': ['Lexend Deca'],
+        },
+        // font 
+        // background
+        // backgroundImage: {
+        //   'bG': "url('/picture/01.png')",
+        // },
+        // background
+      }
+    }
+  }
